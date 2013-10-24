07602561b43e54c9f9edfd666d35ed38bdb455e0
Artur Grabowski <art@blahonga.org>
2013-10-24 13:39:55 +0200

    Move test cases into functions.

--- main/bsearch.go
+++ main/bsearch.go
@@ -21,19 +21,7 @@ func (h headers) Add(k, v string) {
 	h[k] = v
 }
 
-func main() {
-	flag.Parse()
-	if flag.NArg() != 1 {
-		usage()
-	}
-	dbname := flag.Arg(0)
-	in, err := index.Open(dbname)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "bindex.Open: %v\n", err)
-		return
-	}
-	defer in.Close()
-
+func stdtest(in *index.Index) {
 	p := parser.Parse(in, "0 lim:10 count_all(hej) root:10 OR magic:boll status:active")
 	q := p.Stack[0]
 
@@ -55,4 +43,41 @@ func main() {
 	for k, v := range h {
 		fmt.Printf("info:%v:%v\n", k, v)
 	} 
+}
+
+func bltest(in *index.Index) {
+	p := parser.Parse(in, "0 count_all(hej) region:11 category:1000 OR category:2000 OR category:3000")
+	q := p.Stack[0]
+
+	ops.Dump(q, 0)
+
+	d := index.NullDoc()
+	for true {
+		d = q.NextDoc(d)
+		if d == nil {
+			break
+		}
+		d = d.Inc()
+	}
+	h := make(headers)
+	q.ProcessHeaders(h)
+	for k, v := range h {
+		fmt.Printf("info:%v:%v\n", k, v)
+	}	
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		usage()
+	}
+	dbname := flag.Arg(0)
+	in, err := index.Open(dbname)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "bindex.Open: %v\n", err)
+		return
+	}
+	defer in.Close()
+
+	bltest(in)
 }
