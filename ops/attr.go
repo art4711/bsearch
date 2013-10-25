80f2995085e194f4e40387fa17d71c36431c0fef
Artur Grabowski <art@blahonga.org>
2013-10-25 14:50:05 +0200

    Inline expand sort.Search for a 30% performance increase.
    We are now just half as fast as the C search engine.

--- ops/attr.go
+++ ops/attr.go
@@ -2,7 +2,6 @@ package ops
 
 import (
 	"bsearch/index"
-	"sort"
 )
 
 type attr []index.IbDoc
@@ -22,15 +21,26 @@ func (ba attr) CurrentDoc() *index.IbDoc {
 
 func (ba *attr) NextDoc(search *index.IbDoc) *index.IbDoc {
 	l := len(*ba)
+
+/*
 	i := sort.Search(l, func(i int) bool {
 		d := (*ba)[i]
-		if search.Order > d.Order {
-			return true
-		} else if search.Order == d.Order {
-			return search.Id >= d.Id
-		}
-		return false
+		return search.Order > d.Order || (search.Order == d.Order && search.Id >= d.Id)
 	})
+ The code below is an expanded version of this call. Inlining gives us a 30% speedup.
+*/
+	i, j := 0, l
+	for i < j {
+		h := i + (j - i)/2
+		d := (*ba)[h]
+		if (search.Order > d.Order || (search.Order == d.Order && search.Id >= d.Id)) {
+			j = h
+		} else {
+			i = h + 1
+		}
+	}
+	/* End of inline expanded sort.Search */
+
 	if i == l {
 		ba = nil
 		return nil
