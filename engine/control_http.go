9b21cc3c11887f2e834b485f14efc28118e6a0cd
Artur Grabowski <art@blahonga.org>
2013-11-15 09:46:13 +0100

    Use the JSON handler from the timers package.

--- engine/control_http.go
+++ engine/control_http.go
@@ -5,8 +5,6 @@ package engine
 
 import (
 	"net/http"
-	"encoding/json"
-	"fmt"
 	"github.com/art4711/timers"
 )
 
@@ -15,7 +13,13 @@ func (s EngineState) ControlHTTP(cchan chan string) {
 	mux.HandleFunc("/stop", func(w http.ResponseWriter, req *http.Request) {
 		cchan <- "stop"
 	})
-	mux.HandleFunc("/timers", s.handleTimers)
+	mux.HandleFunc("/timers", func (w http.ResponseWriter, req *http.Request) {
+		s.Timer.JSONHandler(w, req)
+	})
+	mux.HandleFunc("/graph", func (w http.ResponseWriter, req *http.Request) {
+		timers.JSONHandlerGraph(w, req, "/timers")
+	})
+
 	addr := ":" + s.Conf.GetString("port", "command")
 	hs := &http.Server{
 		Addr: addr,
@@ -24,35 +28,3 @@ func (s EngineState) ControlHTTP(cchan chan string) {
 	hs.ListenAndServe()
 }
 
-func (s EngineState) handleTimers(w http.ResponseWriter, req *http.Request) {
-	data := make(map[string]interface{})
-
-	req.ParseForm()
-	var filt string
-	if len(req.Form["filt"]) > 0 && len(req.Form["filt"][0]) > 0 {
-		filt = req.Form["filt"][0]
-	}
-
-	s.Timer.Foreach(func (name []string, cnt *timers.Counts) {
-		if filt != "" && len(name) > 0 && name[0] != filt {
-			return
-		}
-		dp := data
-		for _, v := range name {
-			if _, exists := dp[v]; !exists {
-				dp[v] = make(map[string]interface{})
-			}
-			dp = dp[v].(map[string]interface{})
-		}
-		dp["cnt"] = fmt.Sprint(cnt.Count)
-		dp["tot"] = cnt.Tot.String()
-		dp["min"] = cnt.Min.String()
-		dp["avg"] = cnt.Avg.String()
-		dp["max"] = cnt.Max.String()
-		dp["numgc"] = fmt.Sprint(cnt.NumGC)
-		dp["bytes"] = fmt.Sprint(cnt.BytesAlloc)
-		dp["gctime"] = fmt.Sprint(cnt.GCTime)
-	})
-	json, _ := json.MarshalIndent(data, "", "    ")
-	w.Write(json)
-}
